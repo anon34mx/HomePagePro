8f7417ecbcd7393a38929b0014dd063ac53f3b94
anon34mx <a.non34@hotmail.com>
2021-06-16 18:24:03 -0500

    #
    Funciona

--- main.js
+++ main.js
@@ -7,35 +7,65 @@ var app = express();
 let ejs = require('ejs');
 app.set('view engine', 'ejs');
 
-var filesFound="",folders="";
+var filesFound="",folders="",shortcuts="x";
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', async function(req, res) {
     filesFound="";
     folders="";
+    // shortcuts="";
     console.log("START___"+new Date().getTime());
-    await promesa(lhl);
-    console.log(filesFound);
-    console.log(folders);
+    await Shortcuts();
+    await scanFolder(lhl);
     console.log("END_____"+new Date().getTime());
+
     res.render('index', {
         filesFound:filesFound,
-        folders:folders
+        folders:folders,
+        shortcuts:shortcuts,
     });
+});
+app.get('/open', function(req, res) {
+    // console.log(req);
+    // "D:\xampp\htdocs\EV 12 corregido_v4.rar"
+    // require('child_process').exec('start "" "D:/xampp/htdocs/EV 12 corregido_v4.rar"');
+    // res.render('index', {
+    //     filesFound:filesFound,
+    //     folders:folders
+    // });
 });
 
 app.listen(8080,()=>{});
 
-function promesa(path){
+async function Shortcuts(){
+    shortcuts="";
+    return await new Promise((resolve,reject)=>{
+        fs.readFile("./shortcuts.json","utf8",async (err,jsonString)=>{
+            jsonString=JSON.parse(jsonString)
+            
+            await jsonString.forEach(element => {
+                if(element.type=="group"){
+                    shortcuts+=renderUriGroup(element);
+                }else{
+                    shortcuts+=renderUri(element);
+                }
+            });
+            resolve();
+        });
+    });
+
+}
+function scanFolder(path){
     return new Promise(async (resolve,reject)=>{
-        await fs.readdir(path, async function (err, files) {
+        fs.readdir(path, async function (err, files) {
             if (err) {
                 return console.log('Unable to scan directory: ' + err);
             } 
             //listing all files using forEach
-            await files.forEach(function (file) {
+            files.forEach(function (file) {
                 if(fs.lstatSync(path+"/"+file).isDirectory()){
-                    folders+=path+"/"+file+"<br>";
+                    // folders+=path+"/"+file+"<br>";
+                    folders+=renderFolder(path,file);
                 }else{
                     // filesFound+=path+"/"+file+"<br>";
                     filesFound+=renderFile(path,file);
@@ -45,16 +75,26 @@ function promesa(path){
         });
     })
 }
-function renderFolder(){
-    
+function renderFolder(path,file){
+    return `<div class='element folder'>
+        `+file+`
+    </div>`;
 }
 function renderFile(path,file){
     // return `<a href="http://localhost/`+file+`">
-    return `<a href="`+path+`/`+file+`">
+    return `<a class="element file" href='http://localhost:8080/open?file=`+path+"/"+file+`'>
         asd
-    </a>`
+    </a>`;
 }
-function renderUri(){
+function renderUriGroup(element){
+    return `
+        <div class="element uriGroup">`+element.name+`</div>
+    `;
+}
+function renderUri(element){
+    return `
+        <div class="element uri">`+element.name+`</div>
+    `;
 
 }
 // async function scanDir(path) {
