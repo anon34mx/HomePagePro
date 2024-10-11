37062c8230d1811ea366e1f58c42cfcff4cea3f3
aaron <a.non34@hotmail.com>
2024-10-11 02:18:18 -0600

    css
    Glassmorphism

--- themes/glass_default/renders.js
+++ themes/glass_default/renders.js
@@ -2,12 +2,12 @@
 const renderUri=(id, uri, icon, name, blank)=>{
     // <div class="imgContainer">
     // </div>
-    return `<a class="element">
+    return `<a href="`+uri+`"class="element">
         <div class="icon">
-            <img class="" src="http://localhost:34/assets/styles/default/html.svg"/>
+            <img class="" src="`+icon+`" onerror="http://localhost:34/assets/styles/default/html.svg"/>
         </div>
         <label class="txt-white-shadow">
-            nombre del sitio web uwu
+           `+name+`
         </label>
         <div class="bg"></div>
     </a>`;
